172bfbcfe8be859b2e75dc10003d2d2d7b454469
Raghav <157412106+rdheri@users.noreply.github.com>
2026-08-09 22:34:29 -0400

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    count++;
+                    dfs(i, j, grid, visited);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(int i, int j, char[][] grid, boolean[][] visited) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length 
+            || grid[i][j] == '0' || visited[i][j]) return;
+
+        visited[i][j] = true;
+
+        dfs(i+1, j, grid, visited);
+        dfs(i-1, j, grid, visited);
+        dfs(i, j+1, grid, visited);
+        dfs(i, j-1, grid, visited);
+    }
+}
