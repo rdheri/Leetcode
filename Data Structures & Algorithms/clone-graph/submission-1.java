d95474e331547a79277b07b44b9fd6fec7e0d3e6
Raghav <157412106+rdheri@users.noreply.github.com>
2026-08-09 22:45:19 -0400

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.java
@@ -0,0 +1,39 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        HashMap<Integer, Node> map = new HashMap<>();
+        return dfs(node, map);
+    }
+
+    public Node dfs(Node node, HashMap<Integer, Node> map) {
+        if(node == null) return null;
+        if(map.containsKey(node.val)) {
+            return map.get(node.val);
+        }
+        Node clone = new Node(node.val);
+        map.put(node.val, clone);
+        for(Node neighbor : node.neighbors) {
+            clone.neighbors.add(dfs(neighbor, map));
+        }
+        return clone;
+    }
+}
