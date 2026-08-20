ecf4b5c3eb0f99e499ae3823663324b849d078be
Raghav <157412106+rdheri@users.noreply.github.com>
2026-08-20 00:33:38 -0400

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        // n = 7;
+
+        // - - - - - - -
+
+        // number of ways to get to n = n-1 + n-2
+
+        int [] dp = new int [n+1];
+        if(n == 1) return 1;
+        if(n == 2) return 2;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i < n+1; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[dp.length-1];
+    }
+}
